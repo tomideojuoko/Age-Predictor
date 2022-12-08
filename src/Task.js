68e903ac33095eb6d72bcfc39311b6990db74b90
Tomide Ojuoko <tomideojuoko@gmail.com>
2022-12-08 13:38:39 +0100

    To-do List

--- src/Task.js
+++ src/Task.js
@@ -2,6 +2,7 @@ export const Task = (props) => {
     return (
      <div className="task">
        <h1>{props.taskName}</h1>
+       <button> Complete </button>
        <button onClick={() => props.deleteTask(props.id)}> x </button>
      </div> 
   );
